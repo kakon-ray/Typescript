26ff9f52901e91d5adf847b59b4d3af6622ada15
Kakon Ray <thisiskakonray@gmail.com>
2024-11-03 14:52:08 +0600

    Constraint useing Key part 2.7 Completed

--- module2/src/2.6.ts
+++ module2/src/2.6.ts
@@ -1,5 +1,28 @@
 // Constraints in typescript
 
 {
-    
+    const addCourseToStudent = <T extends {id:number,name:string,email:string}>(student:T) => {
+        const course = 'Next Level Web Development'
+
+        return{
+            ...student,
+            course
+        }
+    }
+
+
+
+    const student1 = addCourseToStudent<{
+        id: number;
+        name: string;
+        email: string;
+        devType: string;
+    }>({id:222,name:'Kakon Ray',email:'thisiskakonray@gmail.com',devType:'NLWD'});
+
+    const student2 = addCourseToStudent<{
+        id: number;
+        name: string;
+        email: string;
+        hasWatch: string;
+    }>({id:223,name:'Mr.X',email:'thisiskakonray2@gmail.com',hasWatch:'Apple Watch'});
 }
