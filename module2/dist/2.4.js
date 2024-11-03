71c05dd2eb382f166fa90e0d5be9b96fc931177e
Kakon Ray <thisiskakonray@gmail.com>
2024-11-03 13:01:40 +0600

    2.5 Generic with function completed

---
+++ module2/dist/2.4.js
@@ -0,0 +1,34 @@
+"use strict";
+{
+    const poorDeveloper = {
+        name: 'Kakon Ray',
+        computer: {
+            brand: 'Asush',
+            model: 'X-255uR',
+            releaseYear: 2023
+        },
+        smartWatch: {
+            brand: 'Emilab',
+            model: 'kwee44',
+            display: 'OlED'
+        }
+    };
+    const richDeveloper = {
+        name: 'Kakon Ray',
+        computer: {
+            brand: 'Apple',
+            model: 'X-255uR',
+            releaseYear: 2024
+        },
+        smartWatch: {
+            brand: 'Apple Watch',
+            model: 'kwee44',
+            heartTrack: true,
+            sleepTrack: true
+        },
+        bike: {
+            model: 'Yamaha',
+            EngineCapacity: '150cc'
+        }
+    };
+}
---
+++ module2/dist/2.5.js
@@ -0,0 +1,29 @@
+"use strict";
+// function with generics
+// {
+//     const createArray = (param:string) : string[] => {
+//         return [param]
+//     }
+//     const createArrayWithGeneric = <T>(param:T) : T[] => {
+//         return [param]
+//     }
+//     const res1 = createArray('Bangladesh')
+//     const resGeneric = createArrayWithGeneric<string>('Bangladesh')
+//     type User = {Id:number,name:string}
+//     const resGenericObject = createArrayWithGeneric<User>({Id:222,name:'Kakon'})
+// }
+{
+    // function with generics
+    const createArrayWithTuple = (param1, param2) => {
+        return [param1, param2];
+    };
+    const resGeneric = createArrayWithTuple('Bangladesh', 222);
+    const resGenericObject = createArrayWithTuple('Bangladesh', { Id: 222, name: 'Kakon' });
+    const addCourseToStudent = (student) => {
+        const course = 'Next Level Web Development';
+        return Object.assign(Object.assign({}, student), { course });
+    };
+    const student1 = addCourseToStudent({ name: 'Kakon Ray', email: 'thisiskakonray@gmail.com', devType: 'NLWD' });
+    const student2 = addCourseToStudent({ name: 'Mr.X', email: 'thisiskakonray2@gmail.com', devType: 'NLWD' });
+    console.log(student1);
+}
